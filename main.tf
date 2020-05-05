cb5202c5577ce59dd9445dffc894e29fe3fb95c9
C Wills <64535365+wills-gft@users.noreply.github.com>
2020-05-05 15:09:54 +0100

    Inital terraform commit for module and main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+
+module "Webserver" {
+  source = "./modules/service/webserver-cluster"
+  cluster_name = foo
+  min_size     = 1
+  max_size     = 5
+  instance_type = "t2.micro"
+
+}
